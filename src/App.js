3e3400119826db3e3959b3618e0779c0022cda9b
ArshdeepSingh <deepsingh13131212@gmail.com>
2024-07-09 04:11:29 +0530

    generate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [password, setPassword] = useState("");
+  let [length, setLength] = useState(8);
+  let [isNumber, setIsnumber] = useState(true);
+  let [isCharacter, setIscharacter] = useState(true);
+  let passref = useRef(null);
+
+  const handler=useCallback(()=>{
+    
+    passref.current?.select();
+    window.navigator.clipboard.writeText(password);
+  
+},[password])
+
+  const generate = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLPMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (isNumber) { str += "1234567890" };
+    if (isCharacter) { str += "!@#$%^&*(){}:<>][|.<>" };
+
+
+    for (let i = 0; i < length; i++) {
+      let ram = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(ram);
+
+    }
+
+    setPassword(pass)
+
+  }, [setPassword, length, isCharacter, isNumber])
+
+  useEffect(() => { generate() }, [generate])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>
+        Password Generator
+      </h1>
+      <label>Password</label><input value={password} type='text' ref={passref} />
+      <label> <br />Length : {length} <br /></label><input onChange={(e) => {
+        setLength(e.target.value)
+      }} value={length} type='range' />
+      <label>Numbers</label><input checked={(isNumber)} onClick={() => {
+        setIsnumber((prev) => !prev)
+      }} value={isNumber} type='checkbox' />
+      <label>Characters</label><input checked={(isCharacter)} onChange={() => {
+        setIscharacter((prev) => !prev)
+      }} value={isCharacter} type='checkbox' />
+      <button style={{ cursor: 'pointer' }} onClick={handler}>Copy</button>
+    </>
   );
 }
 
